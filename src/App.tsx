7fe87efd705e834cb839fe373fc519fa30280ed3
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2025-12-24 00:27:16 +0300

    Add lesson 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+// Импортируем BrowserRouter, Route, Routes для настройки маршрутизации
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Layout from "components/Layout/Layout";
+import ContactUs from "pages/EmployeeApp/ContactUs/ContactUs";
+import Home from "pages/EmployeeApp/Home/Home";
+import LogIn from "pages/EmployeeApp/LogIn/LogIn";
+import About from "pages/EmployeeApp/About/About";
+
 import GlobalStyles from "styles/GlobalStyles";
 
 // Lessons
@@ -14,23 +23,34 @@ import Homework_07 from "homeworks/Homework_07/Homework_07";
 import Homework_08 from "homeworks/Homework_08/Homework_08";
 import Homework_09 from "homeworks/Homework_09/Homework_09";
 import Homework_10 from "homeworks/Homework_10/Homework_10";
+import Homework_12 from "homeworks/Homework_12/Homework_12";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+      <GlobalStyles />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/contactUs" element={<ContactUs />} />
+          <Route path="*" element="Page Not Found" />
+        </Routes>
+      </Layout>
       {/* <Lesson_06 /> */}
       {/* <Homework_06 /> */}
       {/* <Homework_07 /> */}
       {/* <Lesson_07 /> */}
-      <GlobalStyles />
       {/* <Lesson_08 /> */}
       {/* <Homework_08 /> */}
       {/* <Lesson_09 /> */}
       {/* <Homework_09 /> */}
       {/* <Lesson_10 /> */}
       {/* <Homework_10 /> */}
-      <Lesson_12 />
-    </>
+      {/* <Lesson_12 /> */}
+      {/* <Homework_12 /> */}
+    </BrowserRouter>
   );
 }
 
